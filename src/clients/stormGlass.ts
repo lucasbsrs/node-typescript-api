b9f57d7f26200dbc14529395e365329264cb217e
Lucas Batista da Silva <lucas.batista@paipe.co>
2020-08-11 00:03:27 -0300

    Executado prettier

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosStatic } from 'axios';
 
 export class StormGlass {
-
-  readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaaa';
 
   constructor(protected request: AxiosStatic = axios) {}
@@ -10,7 +10,6 @@ export class StormGlass {
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
     return this.request.get(`
       https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}
-    `)
+    `);
   }
-
 }
